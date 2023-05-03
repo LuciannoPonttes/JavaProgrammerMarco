0871ccfc33b5eea5a784d2363504ef0400491db7
luciannoponttes <luciannoponttes@gmail.com>
2023-05-03 20:49:41 -0300

    Matrix uni e bi

---
+++ Aula-03-05-23/matrix-tabela/src/repositorio/PessoaInterface.java
@@ -0,0 +1,15 @@
+package repositorio;
+
+import java.util.List;
+
+import entidades.Pessoa;
+
+public interface PessoaInterface {
+
+	
+	public void salvar(Pessoa pessoa);
+	
+	public List<Pessoa> retonarListaPessoas();
+	
+	
+}
